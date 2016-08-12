417da68f65a3fcece3cdddb818461f02ee59789e
Rigel Doria <rcbd@codemagnus.com>
2016-08-12 22:25:11 +0800

    Initialize repo

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require ('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		//new Date().toString()
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+}; 
+
+//app.use(middleware.requireAuthentication);
+//ALL CAPS value should be constant
+app.use(middleware.logger);
+
+//request
+//response
+
+
+//about
+app.get ('/about', middleware.requireAuthentication, function (req, res) {
+	res.send ('About us');
+});
+
+app.use(express.static(__dirname = 'public'));
+
+
+/*app.listen(3000, function () {
+	console.log('Express server started!');
+});*/
+
+app.listen(PORT, function () {
+	console.log('Express server started at port ' + PORT + '!');
+});
+
